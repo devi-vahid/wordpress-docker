94df22cbeea31d512e02e0a2d8e5a4f569af5267
Devi <vahid.m.dev@gmail.com>
2022-09-01 19:22:03 +0430

    html directroy will be part of the repo fix permissions script will only change required files and directories

--- fix-permission.sh
+++ fix-permission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # from https://medium.com/@nielssj/772c1aee23ca
-sudo chown -R $USER:1024 ./*
-sudo chmod -R 775   ./*
-sudo chmod -R g+s   ./*
+sudo chown -R $USER:1024 ./html ./data/logs/
+sudo chmod -R 775   ./html ./data/logs/
+sudo chmod -R g+s   ./html ./data/logs/

5c981f364104b31f8d69b18da269315dcce495c4
Devi <vahid.m.dev@gmail.com>
2022-09-06 03:02:37 +0430

    unsupport/unused options removed from docker-compose file fix-permission.sh no longer required Dockerfile will now create a user and group with id of 1000

--- fix-permission.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# from https://medium.com/@nielssj/772c1aee23ca
-sudo chown -R $USER:1024 ./html ./data/logs/
-sudo chmod -R 775   ./html ./data/logs/
-sudo chmod -R g+s   ./html ./data/logs/
